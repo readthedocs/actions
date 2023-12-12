69903942fe32025af57a985dc81b024eb57d8029
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2023-12-12 09:58:37 +0100

    Add minimal .readthedocs.yaml (#33)
    * Add minimal .readthedocs.yaml
    
    The default conf.py is no longer
    injected.
    
    * Update .readthedocs.yaml
    
    Co-authored-by: Ege Akman <egeakmanegeakman@hotmail.com>
    
    ---------
    
    Co-authored-by: Manuel Kaufmann <humitos@gmail.com>
    Co-authored-by: Ege Akman <egeakmanegeakman@hotmail.com>

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+# -- General configuration ------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = []
+
+project = 'Read the Docs'
+copyright = '2023, Read the Docs'
+author = ''
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# These patterns also affect html_static_path and html_extra_path
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output ----------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
